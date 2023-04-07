c130caeef2c7f9ba5d16c140d75239c2ff5067e3
FandiNurrokhim <109939812+FandiNurrokhim@users.noreply.github.com>
2023-04-07 15:04:00 +0700

    Add files via upload

---
+++ apotek.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2023 at 10:02 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pembelian`
+--
+
+CREATE TABLE `detail_pembelian` (
+  `id_pembelian` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `jumlah_bayar` int(11) DEFAULT NULL,
+  `kembalian` int(11) DEFAULT NULL,
+  `tanggal_transaksi` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_pembelian`
+--
+
+INSERT INTO `detail_pembelian` (`id_pembelian`, `id_pembeli`, `id_barang`, `jumlah`, `harga`, `total`, `jumlah_bayar`, `kembalian`, `tanggal_transaksi`) VALUES
+(4, 1, 4, 2, 20000, 40000, 50000, 10000, '2023-04-06');
+
+--
+-- Triggers `detail_pembelian`
+--
+DELIMITER $$
+CREATE TRIGGER `set_kembalian` AFTER INSERT ON `detail_pembelian` FOR EACH ROW UPDATE detail_pembelian
+SET kembalian = jumlah_bayar-total
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `set_total_transaksi` AFTER INSERT ON `detail_pembelian` FOR EACH ROW UPDATE detail_pembelian SET total=jumlah*harga
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `udpate_saldo` AFTER INSERT ON `detail_pembelian` FOR EACH ROW UPDATE pembeli, detail_pembelian
+SET pembeli.saldo = pembeli.saldo - detail_pembelian.total
+WHERE pembeli.id_pembeli = detail_pembelian.id_pembeli
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_stok_barang` AFTER INSERT ON `detail_pembelian` FOR EACH ROW UPDATE obat SET stok_obat = stok_obat - NEW.jumlah WHERE id_obat = NEW.id_barang
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `nama_obat` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL,
+  `harga_obat` int(11) NOT NULL,
+  `stok_obat` int(11) NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama_obat`, `gambar`, `kategori`, `harga_obat`, `stok_obat`, `deskripsi`) VALUES
+(2, 'Paramex', 'default-image.jpg', 'ssss', 2111, 2, 'obat'),
+(3, 'Paracetamol', 'bodrexin.jpg', 'aaaaaaaa', 2111, 2, 'asssssssssss'),
+(4, 'obat 3', 'paramex.jpg', 'obat abit', 20000, 18, 'obaaaaaaaaattttttttttt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` int(11) NOT NULL,
+  `nama_pembeli` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_telp` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `saldo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id_pembeli`, `nama_pembeli`, `alamat`, `no_telp`, `email`, `saldo`) VALUES
+(1, 'pembeli1', 'bandar', '08888', 'bandar', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD PRIMARY KEY (`id_pembelian`),
+  ADD KEY `id_pembeli` (`id_pembeli`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id_pembeli`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD CONSTRAINT `detail_pembelian_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`),
+  ADD CONSTRAINT `detail_pembelian_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `obat` (`id_obat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
