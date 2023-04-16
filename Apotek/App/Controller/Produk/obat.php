7e121d3f542a94bbf09c42ef585484fd76742913
FandiNurrokhim <109939812+FandiNurrokhim@users.noreply.github.com>
2023-04-16 17:03:31 +0700

    Add files via upload

---
+++ Apotek/App/Controller/Produk/obat.php
@@ -0,0 +1,87 @@
+<?php
+
+include "koneksi.php";
+
+class Produk {
+    private $conn;
+    public $nama_obat;
+    public $gambar;
+    public $kategori;
+    public $harga_obat; 
+    public $stok_obat; 
+    public $deskripsi;
+
+    public function __construct() {
+        $db = new Koneksi();
+        $this->conn = $db->conn;
+    }
+
+    public function get_obat() {
+        $sql = "SELECT * FROM obat";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public function get_obat_by_id($id) {
+        $sql = "SELECT * FROM obat WHERE id_obat='$id'";
+        $result = $this->conn->query($sql);
+    
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        } else {
+            return false;
+        }
+    }
+    
+
+    public function add_obat($nama_obat, $gambar, $kategori, $harga_obat, $stok_obat, $deskripsi) {
+        $sql = "INSERT INTO obat (nama_obat, gambar, kategori, harga_obat, stok_obat, deskripsi) VALUES ('$nama_obat', '$gambar', '$kategori', '$harga_obat', '$stok_obat', '$deskripsi')";
+        $result = $this->conn->query($sql);
+
+        $target_dir = "../View/images/";
+        $target_file = $target_dir . basename($_FILES["gambar"]["name"]);
+        move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function edit_obat($id_obat, $nama_obat, $gambar, $kategori, $harga_obat, $stok_obat, $deskripsi) {
+        $sql = "UPDATE obat SET nama_obat='$nama_obat',gambar='$gambar', kategori='$kategori', harga_obat='$harga_obat', stok_obat='$stok_obat', deskripsi='$deskripsi' WHERE id_obat='$id_obat'";
+        $result = $this->conn->query($sql);
+        
+        $target_dir = "../View/images/";
+        $target_file = $target_dir . basename($_FILES["gambar"]["name"]);
+
+        move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete_obat($id_obat) {
+        $sql = "DELETE FROM obat WHERE id_obat=$id_obat";
+        $result = $this->conn->query($sql);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
---
+++ Apotek/App/View/User/Riwayat_Transaksi/daftar_beli.php
@@ -0,0 +1,65 @@
+<?php
+	// Panggil fungsi untuk mengambil data transaksi
+	session_start();
+	include "../../../Controller/User/pembeli.php";
+	$transaksi = new Pembeli();
+	$row = $transaksi->get_transaksi_by_username($_SESSION['id_pembeli']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!-- Awesome Font -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Detail Produk</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" />
+  </head>
+
+<body>
+<!-- Tampilkan data transaksi dalam tabel -->
+<div class="container my-5">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Barang</th>
+                <th>Gambar</th>
+                <th>Jumlah</th>
+                <th>Harga</th>
+                <th>Total</th>
+                <th>Tanggal Pembelian</th>
+            </tr>
+        </thead>
+		<tbody>
+				<tr>
+					<td><?= $row['nama_obat'] ?></td>
+					<td><img src="../../Info_Produk/images/<?= $row['gambar'] ?>" alt="" style="width: 100px; height: auto;"></td>
+					<td><?= $row['jumlah'] ?></td>
+					<td><?= $row['harga'] ?></td>
+					<td><?= $row['total'] ?></td>
+					<td><?= $row['tanggal_transaksi'] ?></td>
+				</tr>
+		</tbody>
+
+    </table>
+</div>
+</body>
+</html>
+
+</body>
+</html>
